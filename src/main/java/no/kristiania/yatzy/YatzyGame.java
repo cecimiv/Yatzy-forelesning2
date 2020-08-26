272f5d88eaccb40c05ac65dbdd7fd82522fca636
cecimiv <ceciversen@gmail.com>
2020-08-26 12:37:23 +0200

    jmospca

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -3,6 +3,12 @@ package no.kristiania.yatzy;
 public class YatzyGame {
     public int score(String category, int[] dice) {
         int result = 5;
+        for (int die : dice) {
+            if(die == 1){
+                result += 1;
+            }
+        }
+
         return 5;
     }
 }
--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -7,7 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class YatzyTest {
     @Test
     void shouldScoreOnce(){
+
         assertEquals(5, new YatzyGame().score("ONES", new int[]{1, 1, 1, 1, 1}));
         assertEquals(3, new YatzyGame().score("ONES", new int[]{1, 2, 3, 1, 1}));
+    }
+
+    @Test
+    void shouldScoreTwos(){
+        assertEquals(10, new YatzyGame().score("ONES", new int[]{2, 2, 2, 2, 2}));
+        assertEquals(4, new YatzyGame().score("ONES", new int[]{1, 2, 3, 1, 2}));
     }
 }
